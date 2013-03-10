f285644bd9d740b6db8a09b95b2b7f4a4d1024fd
Adam Ochonicki <adam@fromonesrc.com>
2013-03-10 19:27:19 -0400

    specs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,11 @@
+# Public: Install Cyberduck.app into /Applications.
+#
+# Examples
+#
+#   include cyberduck
 class cyberduck {
   package { 'cyberduck':
     provider   => 'compressed_app',
-    source => 'http://cyberduck.ch/Cyberduck-4.2.1.zip'
+    source     => 'http://cyberduck.ch/Cyberduck-4.2.1.zip'
   }
 }
---
+++ spec/classes/cyberduck_spec.rb
@@ -0,0 +1,6 @@
+require 'spec_helper'
+
+describe 'cyberduck' do
+  it do
+  end
+end
