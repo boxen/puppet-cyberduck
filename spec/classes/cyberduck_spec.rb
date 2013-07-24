236f63d7c15a84b939cfefa5541c17cc090e84fd
Jozef Izso <jozef.izso@gmail.com>
2013-07-24 10:04:40 +0200

    Cyberduck v4.3.1

--- spec/classes/cyberduck_spec.rb
+++ spec/classes/cyberduck_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'cyberduck' do
   it do
     should contain_package('Cyberduck').with({
-      :source   => 'http://cyberduck.ch/Cyberduck-4.2.1.zip',
+      :source   => 'http://cyberduck.ch/Cyberduck-4.3.1.zip',
       :provider => 'compressed_app'
     })
   end
