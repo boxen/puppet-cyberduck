01f49c1a2a0d3d94c89a39480a68e3310dcdc41a
extrinsicmedia <miles@finvfx.com>
2014-10-21 22:14:14 -0700

    Bump version to 4.5.2

--- spec/classes/cyberduck_spec.rb
+++ spec/classes/cyberduck_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'cyberduck' do
   it do
     should contain_package('Cyberduck').with({
-      :source   => 'http://cyberduck.ch/Cyberduck-4.4.3.zip',
+      :source   => 'https://update.cyberduck.io/Cyberduck-4.5.2.zip',
       :provider => 'compressed_app'
     })
   end
