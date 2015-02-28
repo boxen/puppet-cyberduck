37380ca315b7264c4f90df162b407698f7c5867f
Martin Gondermann <magicmonty@users.noreply.github.com>
2015-02-28 10:03:48 +0100

    Update cyberduck_spec.rb

--- spec/classes/cyberduck_spec.rb
+++ spec/classes/cyberduck_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe 'cyberduck' do
-  it do
-    should contain_package('Cyberduck').with({
-      :source   => 'http://cyberduck.ch/Cyberduck-4.3.1.zip',
-      :provider => 'compressed_app'
-    })
+  context 'default version' do
+    it do
+      should contain_package('Cyberduck').with({
+        :source   => 'https://update.cyberduck.io/Cyberduck-4.6.5.zip',
+        :provider => 'compressed_app'
+      })
+    end
   end
 end
